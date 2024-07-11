4af2b7074ec4b991b205a617a148776f89fb0972
mohammed <mohammedradwanrize@gmail.com>
2024-07-11 13:02:51 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+
+from modules import timer
+from modules import initialize_util
+from modules import initialize
+
+startup_timer = timer.startup_timer
+startup_timer.record("launcher")
+
+initialize.imports()
+
+initialize.check_versions()
+
+
+def create_api(app):
+    from modules.api.api import Api
+    from modules.call_queue import queue_lock
+
+    api = Api(app, queue_lock)
+    return api
+
+
+def api_only():
+    from fastapi import FastAPI
+    from modules.shared_cmd_options import cmd_opts
+
+    initialize.initialize()
+
+    app = FastAPI()
+    initialize_util.setup_middleware(app)
+    api = create_api(app)
+
+    from modules import script_callbacks
+    script_callbacks.before_ui_callback()
+    script_callbacks.app_started_callback(None, app)
+
+    print(f"Startup time: {startup_timer.summary()}.")
+    api.launch(
+        server_name=initialize_util.gradio_server_name(),
+        port=cmd_opts.port if cmd_opts.port else 7861,
+        root_path=f"/{cmd_opts.subpath}" if cmd_opts.subpath else ""
+    )
+
+if __name__ == "__main__":
+    from modules.shared_cmd_options import cmd_opts
+    api_only()
